6277b244df11a274e5ef1cfe1bd87ffe387ef102
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-11 14:48:56 +0530

    Fixed the route path

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -89,7 +89,7 @@ const Sidebar = () => {
               </svg>
               Book Room
             </NavLink>
-            <NavLink to="/book-room" className={navLink}>
+            <NavLink to="/booking-history" className={navLink}>
               <svg className="w-4 h-4 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.75}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
